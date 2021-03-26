2fe9a8d3007e1bf293b3068cce5f7b5514fd2d17
Alvaro Curipoma <alvarocuripoma@gmail.com>
2021-03-26 12:27:50 -0500

    Agrege un footer y modifique algunos estilos

--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -9,17 +9,18 @@
 </head>
 <body>
     <div>
-        <header class="w-full px-6 bg-green-200 py-0 flex flex-wrap">
-            <p class="text-base text-white text-4xl italic font-bold">Esquel</p>
+        <header class="w-full px-6 bg-green-200 py-3 flex flex-wrap">
+            <p class="text-white text-4xl italic font-bold">Esquel</p>
+            <a class="text-3xl px-14 text-white font-sans" href="#">cursos</a>
         </header>
 
-        <header  class="w-screen px-6 bg-green py-6 " >
+        <header  class="w-screen px-6 bg-green py-6" >
             <!--======= Logo =========-->
-            <div class="container mx-auto max-w-4xl md:flex justify-between items-center bg-OliveDrab">
+            <div class="container mx-auto max-w-7xl md:flex justify-between items-center bg-OliveDrab">
                 <a href="#" class=" py-6 w-full text-center md:text-left md:w-auto text-gray-600 no-underline  flex justify-center items-center ">
                 <img src="../img/logoEsquel.png">
                 </a>
-                <div class="w-full md:w-auto mb-6 md:mb-0 text-center md:text-right ">
+                <div class="w-full md:w-auto mb-6 md:mb-0 text-center md:text-right">
                     <ul>
                         <li>
                             <lavel >Nombre del usruario : </lavel>
@@ -34,49 +35,48 @@
             </div>
         </header>
         <header>
-            <p class="text-left md:text-center text-5xl font-bold">Cursos Recibidos</p>
+            <p class="container xl:pl-20 lg:text-left text-center text-5xl font-bold px-8 mt-10">Cursos Recibidos</p>
         </header>
 
-        <div class=" container pt-6 ">
+        <div class="mb-80 container py-10 2xl:p-30 xl:p-20">
             <table class="border-collapse border border-green-200">
                 <thead class="text-left border-collapse border border-green-200 bg-green-200">
                     <tr>
-                        <th class="w-1/2 ">Cursos</th>
-                        <th class="w-1/4 ">Horas Realizadas</th>
-                        <th class="w-1/4 ">Descarga</th>
+                        <th class="p-3 w-1/2 ">Cursos</th>
+                        <th class="p-3 w-1/4 ">Horas Realizadas</th>
+                        <th class="p-3 w-1/4 ">Descarga</th>
                     </tr>
                 </thead>
                 <tbody class="border-collapse border border-green-200 ">
                     <tr class="border-collapse border border-green-200 ">
-                        <td>Intro to CSS</td>
-                        <td>Adam</td>
-                        <td>858</td>
+                        <td class="p-3">Intro to CSS</td>
+                        <td class="p-3">Adam</td>
+                        <td class="p-3">858</td>
                     </tr>
                     <tr>
-                        <td>A Long and Winding Tour of the History of UI Frameworks and Tools and the Impact on Design</td>
-                        <td>Adam</td>
-                        <td>112</td>
+                        <td class="p-3">A Long and Winding Tour of the History of UI Frameworks and Tools and the Impact on Design</td>
+                        <td class="p-3">Adam</td>
+                        <td class="p-3">112</td>
                     </tr>
                         <tr class="border-collapse border border-green-200 ">
-                        <td>Intro to JavaScript</td>
-                        <td>Chris</td>
-                        <td>1,280</td>
+                        <td class="p-3">Intro to JavaScript</td>
+                        <td class="p-3">Chris</td>
+                        <td class="p-3">1,280</td>
                     </tr>
                         <tr class="border-collapse border border-green-200 ">
-                        <td>Intro to JavaScript</td>
-                        <td>Alisson</td>
-                        <td>Proceso</td>
+                        <td class="p-3">Intro to JavaScript</td>
+                        <td class="p-3">Alisson</td>
+                        <td class="p-3">Proceso</td>
                     </tr>
                 </tbody>
             </table>
         </div>
-        
+
         <!--======= footer =========-->
-        <footer class="m-20 border border-green-200 py-6">
-            <ul class="grid grid-cols-4 gap-7">
-                <li class="text-center">cursos</li>
-                <li class="text-center">contactos</li>
-                <li class="text-center">redes sociales</li>
+        <footer class="p-0 xl:mx-10 lg:mx-5 md:mx-0">
+            <ul class="p-0 py-2 border border-t-2 border-green-200 border-b-0 border-l-0 border-r-0 grid grid-cols-1 md:grid-cols-2 gap-x-0 gap-y-0 md:gap-y-0">
+                <li class="xl:py-0 lg:py-0 md:py-5 sm:py-5 py-2 text-center"><a href="#">contactos</a></li>
+                <li class="xl:py-0 lg:py-0 md:py-5 sm:py-5 py-2 text-center"><a href="#">redes sociales</a></li>
             </ul>
         </footer>
 
