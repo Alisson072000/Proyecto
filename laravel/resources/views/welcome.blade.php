ef8bd0a99745f7f98fa5d0203fd8579ee53dc777
AlvaroCuripoma <at.curipoma@yavirac.edu.ec>
2021-03-25 23:23:31 -0500

    Agrege un footer

--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -8,73 +8,79 @@
     <title>Talwind</title>
 </head>
 <body>
-<div>
-<header class="w-full px-6 bg-green-200 py-0 flex flex-wrap">
-<p class="text-base text-white text-4xl italic font-bold">Esquel</p>
-    </header>
+    <div>
+        <header class="w-full px-6 bg-green-200 py-0 flex flex-wrap">
+            <p class="text-base text-white text-4xl italic font-bold">Esquel</p>
+        </header>
 
-<header  class="w-screen px-6 bg-green py-6 " >
-      <!--======= Logo =========-->
-      <div class="container mx-auto max-w-4xl md:flex justify-between items-center bg-OliveDrab">
-        <a href="#"
-          class=" py-6 w-full text-center md:text-left md:w-auto text-gray-600 no-underline  flex justify-center items-center ">
-          <img src="img/logoEsquel.png">
-        </a>
-       
-        <div class="w-full md:w-auto mb-6 md:mb-0 text-center md:text-right ">
-          <ul>
-          <li>
-          <lavel >Nombre del usruario : </lavel>
-           <imput>Alisson Chavez</imput>
-           </li>
-           <li>
-           <lavel>Número de Cedula : </lavel>
-           <imput>1755731542</imput>
-           </li>
-           </ul>
+        <header  class="w-screen px-6 bg-green py-6 " >
+            <!--======= Logo =========-->
+            <div class="container mx-auto max-w-4xl md:flex justify-between items-center bg-OliveDrab">
+                <a href="#" class=" py-6 w-full text-center md:text-left md:w-auto text-gray-600 no-underline  flex justify-center items-center ">
+                <img src="../img/logoEsquel.png">
+                </a>
+                <div class="w-full md:w-auto mb-6 md:mb-0 text-center md:text-right ">
+                    <ul>
+                        <li>
+                            <lavel >Nombre del usruario : </lavel>
+                            <imput>Alisson Chavez</imput>
+                        </li>
+                        <li>
+                            <lavel>Número de Cedula : </lavel>
+                            <imput>1755731542</imput>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </header>
+        <header>
+            <p class="text-left md:text-center text-5xl font-bold">Cursos Recibidos</p>
+        </header>
+
+        <div class=" container pt-6 ">
+            <table class="border-collapse border border-green-200">
+                <thead class="text-left border-collapse border border-green-200 bg-green-200">
+                    <tr>
+                        <th class="w-1/2 ">Cursos</th>
+                        <th class="w-1/4 ">Horas Realizadas</th>
+                        <th class="w-1/4 ">Descarga</th>
+                    </tr>
+                </thead>
+                <tbody class="border-collapse border border-green-200 ">
+                    <tr class="border-collapse border border-green-200 ">
+                        <td>Intro to CSS</td>
+                        <td>Adam</td>
+                        <td>858</td>
+                    </tr>
+                    <tr>
+                        <td>A Long and Winding Tour of the History of UI Frameworks and Tools and the Impact on Design</td>
+                        <td>Adam</td>
+                        <td>112</td>
+                    </tr>
+                        <tr class="border-collapse border border-green-200 ">
+                        <td>Intro to JavaScript</td>
+                        <td>Chris</td>
+                        <td>1,280</td>
+                    </tr>
+                        <tr class="border-collapse border border-green-200 ">
+                        <td>Intro to JavaScript</td>
+                        <td>Alisson</td>
+                        <td>Proceso</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
-  
-      </div>
-    </header>
-    <header>
-    <p class="text-left md:text-center text-5xl font-bold">Cursos Recibidos</p>
-    </header>
+        
+        <!--======= footer =========-->
+        <footer class="m-20 border border-green-200 py-6">
+            <ul class="grid grid-cols-4 gap-7">
+                <li class="text-center">cursos</li>
+                <li class="text-center">contactos</li>
+                <li class="text-center">redes sociales</li>
+            </ul>
+        </footer>
 
-<div class=" container pt-6 ">
-<table class="border-collapse border border-green-200 ">
-  <thead class="border-collapse border border-green-200 bg-green-200 ">
-    <tr>
-      <th class="w-1/2 ">Cursos</th>
-      <th class="w-1/4 ">Horas Realizadas</th>
-      <th class="w-1/4 ">Descarga</th>
-    </tr>
-  </thead>
-  <tbody class="border-collapse border border-green-200 ">
-    <tr class="border-collapse border border-green-200 ">
-      <td>Intro to CSS</td>
-      <td>Adam</td>
-      <td>858</td>
-      
-    </tr>
-    <tr>
-      <td>A Long and Winding Tour of the History of UI Frameworks and Tools and the Impact on Design</td>
-      <td>Adam</td>
-      <td>112</td>
-    </tr>
-    <tr class="border-collapse border border-green-200 ">
-      <td>Intro to JavaScript</td>
-      <td>Chris</td>
-      <td>1,280</td>
-    </tr>
-    <tr class="border-collapse border border-green-200 ">
-      <td>Intro to JavaScript</td>
-      <td>Alisson</td>
-      <td>Proceso</td>
-    </tr>
-  </tbody>
-</table>
-</div>
+    </div>
 
-</div> 
 </body>
 </html>
